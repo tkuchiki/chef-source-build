958bcb9f1aad29f877561a5f9f75a004a4399d5c
tkuchiki <kuchiki.taku@gmail.com>
2013-08-30 17:43:24 +0900

    init chef

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'source-build'
+maintainer       'tkuchiki'
+maintainer_email 'kuchiki.taku@gmail.com'
+license          'MIT'
+description      'Installs/Configures source-build'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
